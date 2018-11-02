ee2487aa1144a09b3e64eb43970ad1c59d80f15d
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2018-11-02 09:36:05 +0200

    add solution for problem 3

---
+++ prob_3/prob_3.hs
@@ -0,0 +1,9 @@
+prime_factors :: Int -> [Int]
+
+prime_factors n =
+  case factors of 
+    [] -> [n]
+    _  -> factors ++ prime_factors ((div) n (head factors))
+  where factors = take 1 $ filter (\x -> (==) (mod n x) 0) [2 .. ceiling $ sqrt (fromIntegral (n - 1))]
+
+result = last (prime_factors 600851475143)
