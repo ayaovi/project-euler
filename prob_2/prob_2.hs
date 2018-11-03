73de1a35ac02596feb83e7a8a269b99828f57f52
EspoirDjissenou <ayaovi@users.noreply.github.com>
2018-11-03 17:30:28 +0200

    add problem 2 statement.

--- prob_2/prob_2.hs
+++ prob_2/prob_2.hs
@@ -1,3 +1,4 @@
+-- By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 fib :: [Integer]
 fib = 0 : 1 : zipWith (+) fib (tail fib)
 result = foldl (+) 0 $ filter (\c -> (==) (mod c 2) 0) $ takeWhile ((>) 4000000) fib
