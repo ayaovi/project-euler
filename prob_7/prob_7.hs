58b27dc957d079f9e3d2e2a9c755a0b58c79ced1
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2018-11-05 07:54:52 +0200

    add solution to problem 7.

--- prob_7/prob_7.hs
+++ prob_7/prob_7.hs
@@ -1,1 +1,5 @@
 -- What is the 10 001st prime number?
+is_prime :: Int -> Bool
+is_prime n = (==) [] $ filter (\x -> (==) (mod n x) 0) [2 .. ceiling $ sqrt (fromIntegral (n - 1))]
+
+result = fst $ (filter (\(x,y) -> (==) y True) $ map (\x -> (x, is_prime x)) [1..]) !! 10001
