a7315a5676b364f4d9bd033c07ddad9b82338a1a
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2018-11-06 17:00:10 +0200

    add solution for problem 9.

--- prob_9/prob_9.hs
+++ prob_9/prob_9.hs
@@ -5,3 +5,9 @@
   There exists exactly one Pythagorean triplet for which a + b + c = 1000.
   Find the product abc.
 -}
+
+is_pythagorean_triplet :: Int -> Int -> Int -> Bool
+is_pythagorean_triplet x y z = x * x + y * y == z * z 
+
+ab = head $ dropWhile (\(x, y) -> not $ is_pythagorean_triplet x y (1000 - x - y)) $ concat $ map (\x -> map (\y -> (x, y)) [1..1000]) [1..1000]
+result = fst ab * snd ab * (1000 - fst ab - snd ab)
