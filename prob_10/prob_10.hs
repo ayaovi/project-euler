d6a36a3387a650f1d3f4fe38fa6c3360a1640903
ayaovi <djayespoiro@gmail.com>
2018-11-07 18:17:00 +0200

    add an alternative for computing primes. seems slow.

--- prob_10/prob_10.hs
+++ prob_10/prob_10.hs
@@ -2,4 +2,9 @@
 is_prime :: Int -> Bool
 is_prime n = (==) [] $ filter (\x -> (==) (mod n x) 0) [2 .. ceiling $ sqrt (fromIntegral (n - 1))]
 
+-- primes :: [Int] -> [Int]
+-- primes [] = []
+-- primes (x:xs) = x : primes (filter (\x' -> (/=) (x' `mod` x) 0) xs)
+
 result = foldl (+) 2 $ takeWhile (\x -> (>) 2000000 x) $ filter (\x -> is_prime x) [3,5..]
+-- result = foldl (+) 2 $ primes [3,5..2000000]
