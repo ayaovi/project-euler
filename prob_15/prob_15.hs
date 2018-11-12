df32c3ac05b5629b6e3ac922ca82e5957b0468d7
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2018-11-12 13:24:10 +0200

    add solution to problem 15.

--- prob_15/prob_15.hs
+++ prob_15/prob_15.hs
@@ -1,18 +1,13 @@
 {-
   Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
   How many such routes are there through a 20×20 grid?
-
-   0- 1- 2- 3
-   4- 5- 6- 7
-   8- 9-10-11
-  12-13-14-15
 -}
 
 import Control.Parallel
 
 (|>) = flip ($)
 
-cells = 10
+cells = 11
 size = cells + 1
 
 moves :: Int -> Int -> [[Int]]
@@ -24,4 +19,8 @@ moves x y
   where right = x + 1
         down = x + size
 
-result = (moves 0 (size * size - 1)) |> length
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+result = (factorial 40) `div` (factorial 20 * factorial 20)
