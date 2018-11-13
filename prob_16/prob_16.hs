1d8c0da4c3b627776d96f8b284c77c7848b35f46
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2018-11-13 08:21:39 +0200

    multiplication of multi-digit number by one digit.

--- prob_16/prob_16.hs
+++ prob_16/prob_16.hs
@@ -5,6 +5,17 @@
 
 (|>) = flip ($)
 
-mult :: [Int] -> [Int] -> [Int]
+mult :: [Int] -> Int -> [Int]
+mult x y = (rem:acc)
+            where (acc, rem) = foldr digits ([], 0) z
+                  z = map (\d -> halfMult (d, 0) y) x
+                  digits (lv, lc) (rv, rc) = ((s `mod` r):rv, (s `div` r) + lc)
+                    where s = lv + rc
+                          r = 10
+
+halfMult :: (Int, Int) -> Int -> (Int, Int)
+halfMult (d1, c1) d2 = (s `mod` r, s `div` r)
+                        where s = d1 * d2 + c1
+                              r = 10
 
 res = 2 ** 1000
