27c4dac82f6b5fa773b3856297b3839b0de1de99
ayaovi <djayespoiro@gmail.com>
2018-12-01 07:39:16 +0200

    add main to problem 6.

--- prob_6/prob_6.hs
+++ prob_6/prob_6.hs
@@ -1,4 +1,7 @@
 -- Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-sum_of_square = foldl (+) $ map (\x -> x * x) [1..100]
-s = foldl (+) 0 [1..100]
-result = (-) (s * s) sum_of_square
+main :: IO()
+main = do
+  let s = foldl (+) 0 [x * x | x <- [1..100]]
+  let s' = foldl (+) 0 [1..100]
+  let result = (-) (s' * s') s
+  print result
