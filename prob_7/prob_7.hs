6efbc5dce64852c440253ba2b227f71e5aa52cdd
ayaovi <djayespoiro@gmail.com>
2018-12-01 07:31:14 +0200

    add main to problem 7.

--- prob_7/prob_7.hs
+++ prob_7/prob_7.hs
@@ -2,4 +2,7 @@
 is_prime :: Int -> Bool
 is_prime n = (==) [] $ filter (\x -> (==) (mod n x) 0) [2 .. ceiling $ sqrt (fromIntegral (n - 1))]
 
-result = fst $ (filter (\(x,y) -> (==) y True) $ map (\x -> (x, is_prime x)) [1..]) !! 10001
+main :: IO()
+main = do
+  let result = fst $ (filter (\(x,y) -> (==) y True) $ map (\x -> (x, is_prime x)) [1..]) !! 10001
+  print result
